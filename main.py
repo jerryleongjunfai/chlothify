ac5189e101ab26926c54cda424bd5bea2d088569
Jasmine <jasmineganjy@gmail.com>
2025-07-31 15:45:46 +0800

    jasmine computer edits

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ print("It prints messages to the console.")
 print("Goodbye, World!")
 print("End of script.")
 # This script demonstrates basic print functionality in Python.
+
+print ("test")
+
+print("test")
