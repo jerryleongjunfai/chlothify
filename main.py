148897959094f87919f4556ca53138d724304bd1
Jerry Leong <junfai2007@gmail.com>
2025-07-31 15:30:58 +0800

    another test

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("Hello, World!")
+print("This is a simple Python script.")
+print("It prints messages to the console.")
+print("Goodbye, World!")
+print("End of script.")
+# This script demonstrates basic print functionality in Python.
